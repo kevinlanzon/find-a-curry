93436d102dfd88a4bab572035a6e6de6064c3a7b
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-30 13:17:14 +0100

    added haml and first test now passing

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,15 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    # get 'restaurants' => 'restaurants#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :restaurants
 
   # Example resource route with options:
   #   resources :products do
