7995f3415ab5026663ea9a5fd7b1fbf54c828ece
Kevin Lanzon <kevinlanzon@gmail.com>
2015-05-05 11:58:18 +0100

    added restaurant name uniqueness

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe Restaurant, type: :model do
+
   it { is_expected.to have_many :reviews }
+
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
+  end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Toastie's")
+    restaurant = Restaurant.new(name: "Toastie's")
+    expect(restaurant).to have(1).error_on(:name)
   end
 end
