b8bd1b715838da872fdb4bbc5ac24e19872daf53
Kevin Lanzon <kevinlanzon@gmail.com>
2015-05-05 10:50:02 +0100

    refactored review feature tests using helper method

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -4,23 +4,23 @@ feature 'reviewing' do
   before {Restaurant.create name: 'KFC'}
 
   scenario 'allows users to leave a review using a form' do
-     visit '/restaurants'
-     click_link 'Review KFC'
-     fill_in "Thoughts", with: "Tasty!"
-     select '3', from: 'Rating'
-     click_button 'Leave Review'
+     create_review
      expect(current_path).to eq '/restaurants'
      expect(page).to have_content('Tasty!')
   end
 
   scenario 'a restaurants review is deleted when the restaurant is deleted' do
-     visit '/restaurants'
-     click_link 'Review KFC'
-     fill_in "Thoughts", with: "Tasty!"
-     select '3', from: 'Rating'
-     click_button 'Leave Review'
+     create_review
      expect(page).to have_content('Tasty!')
      click_link 'Delete KFC'
      expect(page).not_to have_content('Tasty!')
+  end
+
+  def create_review
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "Tasty!"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
   end
 end
