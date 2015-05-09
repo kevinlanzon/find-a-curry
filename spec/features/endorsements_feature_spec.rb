5ea3b7fad884eea238b389bdaf632ab9a0b0c7a8
Kevin Lanzon <kevinlanzon@gmail.com>
2015-05-09 17:14:05 +0100

    endorsement tests now failing

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -1,14 +1,30 @@
 require 'rails_helper'
 
 feature 'endorsing reviews' do
+
   before do
+    user = create(:user)
+    login_as(user, :scope => :user)
     kfc = Restaurant.create(name: 'KFC')
-    kfc.reviews.create(rating: 3, thoughts: 'Fried bats?!')
+    kfc.reviews.create(rating: 3, thoughts: 'Nice')
   end
 
-  it 'a user can endorse a review, which increments the endorsement count', js: true do
+  scenario 'a review has a link to endorse a review', js: true do
+    visit '/restaurants'
+    save_and_open_page
+    expect(page).to have_link('Endorse')
+  end
+
+  scenario 'a review display an endorsement associated with it', js: true do
     visit '/restaurants'
     click_link 'Endorse'
-    expect(page).to have_content("1 endorsement")
+    expect(page).to have_content('1 endorsement')
+  end
+
+  scenario 'a review displays multiple endorsements associated with it', js: true do
+    visit '/restaurants'
+    click_link 'Endorse'
+    click_link 'Endorse'
+    expect(page).to have_content('2 endorsements')
   end
 end
